faa47fedb5c5c8bf4298c9103faddb2dfa6cde36
niculas1989 <nicolas.maranzano@libero.it>
2022-03-09 15:27:57 +0100

    added other tries

--- esercizio.sql
+++ esercizio.sql
@@ -5,15 +5,22 @@ WHERE (`date_of_birth`) < '1990';
 
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
-
+SELECT * 
+FROM `courses`
+WHERE `cfu` > '10';
 
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
-
+SELECT * 
+FROM `students`
+WHERE (`date_of_birth`) < '1992-01-01';
 
 
 /*4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 laurea (286)*/
+SELECT * 
+FROM `courses`
+WHERE `period` = 'I semestre' AND `year` = 1;
 
 
 
