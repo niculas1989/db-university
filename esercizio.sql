8ca3b49ff43d5f69292ea7b326bb9a8e77962fe1
niculas1989 <nicolas.maranzano@libero.it>
2022-03-09 15:18:40 +0100

    first try - done

--- esercizio.sql
+++ esercizio.sql
@@ -1,5 +1,7 @@
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
-
+SELECT `date_of_birth` 
+FROM `students` 
+WHERE (`date_of_birth`) < '1990';
 
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
