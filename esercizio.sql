dfce54c14e84025ca97a0932c37c218123de81fc
niculas1989 <nicolas.maranzano@libero.it>
2022-03-09 15:39:31 +0100

    added other exercises

--- esercizio.sql
+++ esercizio.sql
@@ -26,6 +26,10 @@ WHERE `period` = 'I semestre' AND `year` = 1;
 
 /* 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 20/06/2020 (21) */
+SELECT *
+FROM `exams`
+WHERE HOUR(`hour`) > 14
+AND (`date`) = '2020-06-20'
 
 
 
