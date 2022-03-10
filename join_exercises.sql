c4ea43f5b969bf8bf97e1bbcca6b8e3dc1205ed0
niculas1989 <nicolas.maranzano@libero.it>
2022-03-10 15:28:11 +0100

    added changes about first exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,5 +1,9 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
-
+SELECT `students`.*, `degrees`.`name`
+from `degrees`
+JOIN `students`
+ON `degrees`.`id` = `students`.`degree_id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
